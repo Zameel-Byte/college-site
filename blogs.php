996f3e1fd82096eeaba3365d2c16e59c3a2edada
Mohammed Zameel ali <55248408+Zameel-Byte@users.noreply.github.com>
2022-01-13 15:11:32 +0530

    Add files via upload

---
+++ blogs.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0">
+        <title>Pragati Engineering College</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+<script src="https://kit.fontawesome.com/553d2763c4.js" crossorigin="anonymous"></script>
+<!-- <script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="647343089809-81gge9cn48lcug2dvu04opgmb80b0811.apps.googleusercontent.com"> -->
+<style>
+    .top{
+    text-align: center;
+    background-color: #337ab7;
+}
+    @media(max-width:700px){
+        .top{
+        display: block;
+        text-align: center;
+        background-color: #337ab7;
+    }
+    .top img{
+        width:100%
+    }
+}
+</style>
+    </head>
+<body>
+    <div class="top">
+        <img src="cropped-2-1.png" alt="Pragati" >
+    </div>
+    <section class="facility">
+        <h1>Our Blogs</h1>
+        <p>a discussion or informational website published on the World Wide Web consisting of discrete, often informal diary-style text entries (posts)</p>
+        <div class="row">
+            <div class="facility-col">
+                <a href="https://arduino-for-pec.blogspot.com/">
+                <img src="https://www.hw-group.com/files/styles/large/public/press/9010-iot-why-to-use-ethernet/hwgiotplanet.jpg?itok=9PQN8d70">
+                </a>
+                <h3>Arduino</h3>
+                <p>It's Blog written by Pragati student named Zameel Ali</p>
+            </div>
+            <div class="facility-col">
+                <a href="https://www.krishnasbodh.com/">
+                <img src="">
+                </a>
+                <h3></h3>
+                <p>write</p>
+            </div>
+            <div class="facility-col">
+                <a href="https://github.com/just-innovative-bro">
+                <img src="jib.png">
+                </a>
+                <h3>Our GitHub Org</h3> 
+                <p>Github is a social media platform for programers</p>
+            </div>
+        </div>       
+    </section>
+    </body>
+</html>     
---
+++ course.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0">
+        <title>Pragati Engineering College</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+<script src="https://kit.fontawesome.com/553d2763c4.js" crossorigin="anonymous"></script>
+<style>    .top{
+    text-align: center;
+    background-color: #337ab7;
+}</style>
+</head>
+<body>
+    <div class="top">
+        <img src="cropped-2-1.png" alt="Pragathi">
+    </div>
+    <section class="course">
+        <h1>Course We Offer</h1>
+        <p>Under Graduation</p>
+        <div class="row">
+            <div class="course-col">
+                <h3>CSE</h3>
+                <p>write</p>
+            </div>
+            <div class="course-col">
+                <h3>CS-D</h3>
+                <p>write</p>
+            </div>
+            <div class="course-col">
+                <h3>CS-M</h3>
+                <p>write</p>
+            </div>    
+        </div>
+        <div class="row">
+            <div class="course-col">
+                <h3>CS-A</h3>
+                <p>write</p>
+            </div>
+            <div class="course-col">
+                <h3>IT</h3>
+                <p>write</p>
+            </div>
+            <div class="course-col">
+                <h3>ECE</h3>
+                <p>write</p>
+            </div>    
+        </div>
+        <div class="row">
+            <div class="course-col">
+                <h3>EEE</h3>
+                <p>write</p>
+            </div>
+            <div class="course-col">
+                <h3>MECH</h3>
+                <p>write</p>
+            </div>
+            <div class="course-col">
+                <h3>CIVIL</h3>
+                <p>write</p>
+            </div>    
+        </div>    
+    </section>
+</body>
