001faa7519ca48ef00dd64ee52bba61c904489b4
Mohammed Zameel ali <zameelalimohammed282003@gmail.com>
2022-01-14 12:38:55 +0530

    update

--- index.php
+++ index.php
@@ -152,25 +152,6 @@
             navLinks.style.right = "-200px";
         }
     </script>
-    <!-- Google signin JavaScript -->
-    <!-- <script>
-        function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-        }
-    </script> -->
-    <!-- Google signout JavaScript -->
-    <!-- <script>
-        function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-        console.log('User signed out.');
-        });
-        }
-    </script> -->
     <script type="text/javascript">
             var vsid = "kc21509150a6d73";
             (function() { 
