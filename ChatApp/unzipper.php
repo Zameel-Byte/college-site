fedf69788d6b523c767fe43633264440081dc57e
Mohammed Zameel ali <zameelalimohammed282003@gmail.com>
2022-01-15 21:40:48 +0530

    update

--- ChatApp/unzipper.php
+++ ChatApp/unzipper.php
@@ -4,8 +4,6 @@
  * It's handy if you do not have shell access. E.g. if you want to upload a lot
  * of files (php framework or image collection) as an archive to save time.
  * As of version 0.1.0 it also supports creating archives.
- *
- * @author  Andreas Tasch, at[tec], attec.at
  * @license GNU GPL v3
  * @package attec.toolbox
  * @version 0.1.1
