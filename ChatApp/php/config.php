76c0b7e924da1385e878472c9eef63fa6692e178
Mohammed Zameel ali <55248408+Zameel-Byte@users.noreply.github.com>
2022-01-16 20:07:47 +0530

    Update config.php

--- ChatApp/php/config.php
+++ ChatApp/php/config.php
@@ -1,11 +1,16 @@
 <?php
-  $hostname = "studentsconnectedin.database.windows.net";
-  $username = "zameel";
-  $password = "ali$282003";
-  $dbname = "studentsconnectedin/pragati_engineering_college_login";
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:studentsconnectedin.database.windows.net,1433; Database = pragati_engineering_college_login", "zameel", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
-  if(!$conn){
-    echo "Database connection error".mysqli_connect_error();
-  }
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "zameel", "pwd" => "ali$282003", "Database" => "pragati_engineering_college_login", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:studentsconnectedin.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
