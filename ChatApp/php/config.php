57ab0ea3d64e039a04dfc73a677e0f1256d7c759
Mohammed Zameel ali <55248408+Zameel-Byte@users.noreply.github.com>
2022-01-17 11:47:08 +0530

    Update config.php

--- ChatApp/php/config.php
+++ ChatApp/php/config.php
@@ -3,8 +3,9 @@
   $username = "zameel";
   $password = "ali$282003";
   $dbname = "pragati_engineering_college_login";
+  $LoginTimeout = "30";
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  $conn = mysqli_connect($hostname, $username, $password, $dbname, $LoginTimeout);
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
   }
