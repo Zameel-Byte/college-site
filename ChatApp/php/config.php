973718c36f508f1a36c8d7ff10bbcbcb8e520c59
Mohammed Zameel ali <55248408+Zameel-Byte@users.noreply.github.com>
2022-01-17 12:03:42 +0530

    Update config.php

--- ChatApp/php/config.php
+++ ChatApp/php/config.php
@@ -1,5 +1,5 @@
 <?php
-  $hostname = "studentsconnectingin.scm.azurewebsites.net";
+  $hostname = "studentsconnectedin.database.windows.net,1433";
   $username = "zameel";
   $password = "ali$282003";
   $dbname = "pragati_engineering_college_login";
