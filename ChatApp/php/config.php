7961c84835d67b8d00e18a56b92d0b7a5de687da
Zameel Byte <zameelalimohammed282003@gmail.com>
2022-01-18 17:18:10 +0530

    update

--- ChatApp/php/config.php
+++ ChatApp/php/config.php
@@ -3,10 +3,7 @@
   $username = "zameel";
   $password = "ali$282003";
   $dbname = "pragati_engineering_college_login";
-  $logintimeout = "30";
-  $encrypt = "1";
-  $trustservercertificate = "0";
-  $conn = mysqli_connect($hostname, $username, $password, $dbname, $logintimeout, $encrypt, $trustservercertificate);
+  $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
   }
