470b27d455e404710b57c0e06d9378acee213d93
Zameel Byte <zameelalimohammed282003@gmail.com>
2022-01-26 21:25:27 +0530

    files formated

--- course.php
+++ course.php
@@ -1,35 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale = 1.0">
-        <title>Pragati Engineering College</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
-<script src="https://kit.fontawesome.com/553d2763c4.js" crossorigin="anonymous"></script>
-<style>    .top{
-    text-align: center;
-    background-color: #337ab7;
-}</style>
-<style>
-    .top{
-    text-align: center;
-    background-color: #337ab7;
-}
-    @media(max-width:700px){
-        .top{
-        display: block;
-        text-align: center;
-        background-color: #337ab7;
-    }
-    .top img{
-        width:100%
-    }
-}
-</style>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0">
+    <title>Pragati Engineering College</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+    <script src="https://kit.fontawesome.com/553d2763c4.js" crossorigin="anonymous"></script>
+    <style>
+        .top {
+            text-align: center;
+            background-color: #337ab7;
+        }
+    </style>
+    <style>
+        .top {
+            text-align: center;
+            background-color: #337ab7;
+        }
+
+        @media(max-width:700px) {
+            .top {
+                display: block;
+                text-align: center;
+                background-color: #337ab7;
+            }
+
+            .top img {
+                width: 100%
+            }
+        }
+    </style>
 </head>
+
 <body>
     <div class="top">
         <img src="cropped-2-1.png" alt="Pragathi">
@@ -49,7 +55,7 @@
             <div class="course-col">
                 <h3>CS-A&M</h3>
                 <p>Computer Science & Engineering (Artificial Intelligence & Machine Learning)</p>
-            </div>        
+            </div>
         </div>
         <div class="row">
             <div class="course-col">
@@ -62,10 +68,10 @@
             </div>
             <div class="course-col">
                 <a href="https://studymaterial-pec.blogspot.com/">
-                <h3>ECE</h3>
-                <p>Electronics and Communications Engineering</p>
+                    <h3>ECE</h3>
+                    <p>Electronics and Communications Engineering</p>
                 </a>
-            </div>    
+            </div>
         </div>
         <div class="row">
             <div class="course-col">
@@ -79,7 +85,7 @@
             <div class="course-col">
                 <h3>CIVIL</h3>
                 <p>Construction Investigation Various Information Levelling Engineering</p>
-            </div>    
-        </div>    
+            </div>
+        </div>
     </section>
 </body>
